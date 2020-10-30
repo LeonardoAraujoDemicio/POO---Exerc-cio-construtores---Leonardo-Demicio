05efbd1e5da2453eec065fd0edd14ad24b9c1e70
LeonardoAraujoDemicio <69490203+LeonardoAraujoDemicio@users.noreply.github.com>
2020-10-30 19:41:30 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercício_Construtores_Leo_A._Demicio
+{
+    
+
+    public class Produto
+    {
+        // declaração das variáveis.
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        private int quantidade;
+
+        // criação dos métodos do programa.
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProduto(int qte)
+        {
+            Quantidade = Quantidade - qte;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", R$: "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ","
+            +  Quantidade
+            + "unidades, Total: R$ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
